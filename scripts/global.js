188f8cbabc01b6213bd50ce92b9ab49d61d12133
haoqing128 <haoqing128@163.com>
2017-08-10 19:49:56 +0800

    0810

--- scripts/global.js
+++ scripts/global.js
@@ -29,7 +29,7 @@ function addClass(element,value) {
     }
     else{
         newClassName=element.className;
-        newClassName+='';
+        newClassName+=' ';//这里一定要注意是空格！！！
         newClassName+=value;
         element.className=newClassName;
     }
@@ -89,59 +89,6 @@ function prepareInternalnav() {
 }
 addLoadEvent(prepareInternalnav);
 /*photos_图片库*/
-/*function showPic(whichpic) {
-    if(!document.getElementById("placeholder")) return false;
-    var source=whichpic.getAttribute("href");
-    var placeholder=document.getElementById("placeholder");
-    var text;
-    placeholder.setAttribute("src",source);
-    if(!document.getElementById("descirption")) return false;
-    if(whichpic.getAttribute("title"))
-    {
-        text=whichpic.getAttribute("title");
-    }
-    else{
-        text="";
-    }
-    var description=document.getElementById("description");
-    if(description.firstChild.nodeType==3)
-    {
-        description.first.nodeValue=text;
-    }
-    return false;
-}
-function preparePlaceholder() {
-    if(!document.getElementById) return false;
-    if(!document.getElementById("imagegallery")) return false;
-    if(!document.createElement) return false;
-    if(!document.createTextNode) return false;
-    var placeholder=document.createElement("img");
-    placeholder.setAttribute("id","placeholder");
-    placeholder.setAttribute("src","images/photos/placeholder.jpg");
-    placeholder.setAttribute("alt","zhanwei");
-    var description=document.createElement("p");
-    description.setAttribute("id","description");
-    var desctext=document.createTextNode("选择一个图片");
-    description.appendChild(desctext);
-    var gallery=document.getElementById("imagegallery");
-    insertAfter(description,gallery);
-    insertAfter(placeholder,description);
-}
-function prepareGallery() {
-    if(!document.getElementById) return false;
-    if(!document.getElementsByTagName) return false;
-    if(!document.getElementById("imagegallery")) return false;
-    var gallery=document.getElementById("imagegallery");
-    var links=gallery.getElementsByTagName("a");
-    for(var i=0;i<links.length;i++)
-    {
-        links[i].onclick=function () {
-            return showPic(this);
-        }
-    }
-}
-addLoadEvent(preparePlaceholder);
-addLoadEvent(prepareGallery);*/
 function preparePlaceholder(){
     if( !document.getElementById) return false;
     if( !document.getElementsByTagName ) return false;
@@ -189,3 +136,40 @@ function showpic(whichpic){
 }
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
+
+/*live*/
+function stripeTables() {
+    if(!document.getElementsByTagName) return false;
+    var tables=document.getElementsByTagName('table');
+    for(var i=0;i<tables.length;i++) {
+        var odd = false;
+        var rows = tables[i].getElementsByTagName('tr');
+        for (var j = 0; j < rows.length; j++) {
+            if (odd == true) {
+                addClass(rows[j], 'odd');
+                odd = false;
+            }
+            else {
+                odd = true;
+            }
+        }
+    }
+}
+
+function highlightRows() {
+    if(!document.getElementsByTagName) return false;
+    var rows=document.getElementsByTagName('tr');
+    for(var i=0;i<rows.length;i++) {
+        rows[i].oldCN = rows[i].className;
+        rows[i].onmouseover = function () {
+            addClass(this, 'highlight');
+        }
+        rows[i].onmouseout = function () {
+            this.className = this.oldCN;
+        }
+
+    }
+}
+
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
